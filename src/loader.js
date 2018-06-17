813ccd3cbbc247f6fcd157c3756a47753b956a3c
RDRR7 <rene.d.rendon@unitec.edu>
2018-06-17 15:58:18 -0600

    Added push_duplicates function and draft for dp function

--- src/loader.js
+++ src/loader.js
@@ -7,7 +7,23 @@
 		});
 	}
 
+	function push_duplicates() {
+		items.forEach(item => {
+			for (let i = 1; i < item.amount; i++) {
+				items.push({
+					code: item.code,
+					amount: 1,
+					height: item.height,
+					width: item.width,
+					depth: item.depth,
+				});
+			}
+			item.amount = 1;
+		});
+	}
+
 	module.exports.get_items = function() {
+		push_duplicates();
 		calculate_volume();
 		return items;
 	};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,39 @@
 const loader = require('./loader.js');
 
 const items = loader.get_items();
+
+const cotainer_size = {
+	heigth: 4,
+	width: 4,
+	depth: 4,
+};
+
+const container_volume =
+	cotainer_size.heigth * cotainer_size.width * cotainer_size.depth;
+
+function dp_container(total_volume, items) {
+	const DP = new Array(items.length + 1);
+	for (let i = 0; i < DP.length; i++) {
+		DP[i] = new Array(total_volume + 1);
+	}
+
+	for (let i = 0; i < items.length + 1; i++) {
+		for (let j = 0; j < total_volume + 1; j++) {
+			if (i === 0 || j === 0) {
+				DP[i][j] = 0;
+			} else if (items[i - 1].volume <= j) {
+				DP[i][j] = Math.max(
+					DP[i - 1][j - items[i - 1].volume] + items[i - 1].volume,
+					DP[i - 1][j]
+				);
+			} else {
+				DP[i][j] = DP[i - 1][j];
+			}
+		}
+	}
+	return DP[items.length][total_volume];
+}
+
+console.log(`Container volume: ${container_volume}`);
 console.log(items);
+console.log(dp_container(container_volume, items));
